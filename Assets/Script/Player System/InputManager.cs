243c3597043c08f35d5342148f328464a699f6f2
Trolledguy <popza2177@gmail.com>
2026-01-21 17:28:29 +0700

    Basic Player Movement

---
+++ Assets/Script/Player System/InputManager.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public static InputManager Instance { get; private set; }
+
+    [Header("Input State")]
+    public float turnSpeed;
+    private bool isUsingComputer = false;
+
+    void Awake()
+    {
+        if (Instance != this)
+            Instance = this;
+    }
+
+    void Update()
+    {
+        if(!isUsingComputer)
+        {
+            if(Input.GetKeyDown(KeyCode.A))
+            {
+                StartCoroutine(PlayerCamera.Instance.MoveCamera(MoveDirection.Left, turnSpeed));
+            }
+            else if(Input.GetKeyDown(KeyCode.D))
+            {
+                StartCoroutine(PlayerCamera.Instance.MoveCamera(MoveDirection.Right, turnSpeed));
+            }
+        }
+        
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            if(PlayerCamera.Instance.isCameraZooming()) return;
+            Debug.Log("Interact");
+            StartCoroutine(PlayerCamera.Instance.AdjustFOV(isUsingComputer, 5f));
+            isUsingComputer = !isUsingComputer;
+        }
+    }
+}
---
+++ Assets/Script/Player System/PlayerCamera.cs
@@ -0,0 +1,100 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    public static PlayerCamera Instance { get; private set; }
+    [Header("Camera Settings")]
+    [SerializeField] private Camera playerCamera;
+    private bool isZooming = false;
+
+    void Awake()
+    {
+        SetUp();
+    }
+
+    public IEnumerator MoveCamera(MoveDirection direction, float speed)
+    {
+        if(isZooming) yield break;
+        isZooming = true;
+        switch (direction)
+        {
+            case MoveDirection.Left:
+                Quaternion targetYawL = Quaternion.Euler(playerCamera.transform.localEulerAngles.x, playerCamera.transform.localEulerAngles.y - 90, playerCamera.transform.localEulerAngles.z);
+                while(true)
+                {
+                    playerCamera.transform.localEulerAngles = Quaternion.RotateTowards(playerCamera.transform.rotation, targetYawL, speed * Time.deltaTime).eulerAngles;
+                    if(Quaternion.Angle(playerCamera.transform.rotation, targetYawL) < 0.1f)
+                    {
+                        isZooming = false;
+                        yield break;
+                    }
+                    Debug.Log(Quaternion.Angle(playerCamera.transform.rotation, targetYawL));
+                    yield return null;
+                }
+            case MoveDirection.Right:
+                Quaternion targetYawR = Quaternion.Euler(playerCamera.transform.localEulerAngles.x, playerCamera.transform.localEulerAngles.y + 90, playerCamera.transform.localEulerAngles.z);
+                while(true)
+                {
+                    playerCamera.transform.localEulerAngles = Quaternion.RotateTowards(playerCamera.transform.rotation, targetYawR, speed * Time.deltaTime).eulerAngles;
+                    if(Quaternion.Angle(playerCamera.transform.rotation, targetYawR) < 0.1f)
+                    {
+                        isZooming = false;
+                        yield break;
+                    }
+                    Debug.Log(Quaternion.Angle(playerCamera.transform.rotation, targetYawR));
+                    yield return null;
+                }            
+        }
+        yield return null;
+    }
+
+    public IEnumerator AdjustFOV(bool isOnComputer, float speed)
+    {
+        if(isZooming) yield break;
+        isZooming = true;
+
+        if(!isOnComputer) 
+        {
+
+            while(true)
+            {
+                playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, 29 , speed * Time.deltaTime);
+                if(Mathf.Abs(playerCamera.fieldOfView - 29) < 0.1f)
+                {
+                    isZooming = false;
+                    yield break;
+                }
+                yield return null;
+            }
+        }
+        else
+        {
+
+            while(true)
+            {
+                playerCamera.fieldOfView = Mathf.Lerp(playerCamera.fieldOfView, 60, speed * Time.deltaTime);
+                if(Mathf.Abs(playerCamera.fieldOfView - 60) < 0.1f)
+                {
+                    isZooming = false;
+                    yield break;
+                }
+                yield return null;
+            }
+        }
+    }
+
+    public bool isCameraZooming()
+    {
+        return isZooming;
+    }
+
+    private void SetUp()
+    {
+        if(Instance == null)
+        Instance = this;
+        else
+        Destroy(gameObject);
+    }
+}
