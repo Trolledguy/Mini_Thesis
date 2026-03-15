8262eeee950106acc1a3f5ab78cc959c3c579d30
Trolledguy <popza2177@gmail.com>
2026-03-16 02:00:11 +0700

    Add untrack file

---
+++ Assets/Script/WindowSystem/Shop/SkinSystem/UIObject.cs
@@ -0,0 +1,43 @@
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIObject : Component
+{
+    [Header("List of UI Object")]
+    public Image backGround;
+
+    public void ChangeSkin(UISetInfo uISetInfo)
+    {
+        Catbook catbook = WindowManager.instance.AccessApp(WindowAppType.Catbook).GetComponent<Catbook>();
+        Chat chat = WindowManager.instance.AccessApp(WindowAppType.CatChat).GetComponent<Chat>();
+        Shop shop = WindowManager.instance.AccessApp(WindowAppType.CatShop).GetComponent<Shop>();
+
+        backGround.sprite = uISetInfo.backGround;
+
+        catbook.ChangeSkin(uISetInfo.GetSkinByType(WindowAppType.Catbook));
+        chat.ChangeSkin(uISetInfo.GetSkinByType(WindowAppType.CatChat));
+        shop.ChangeSkin(uISetInfo.GetSkinByType(WindowAppType.CatShop), uISetInfo);
+        
+        FeedTemplate feedTemplate = AssetDatabase.LoadAssetAtPath<FeedTemplate>("Assets/Resources/Prefab");
+        feedTemplate.background.sprite = uISetInfo.feedBackground;
+        feedTemplate.likeButton.image.sprite = uISetInfo.likeButton;
+        EditorUtility.SetDirty(feedTemplate);
+
+        CatProfile catProfile = AssetDatabase.LoadAssetAtPath<CatProfile>("Assets/Resources/Prefab");
+        catProfile.ChangeSkin(uISetInfo);
+        EditorUtility.SetDirty(catProfile);
+
+        ChatBlubbleTemplate chatBlubble = AssetDatabase.LoadAssetAtPath<ChatBlubbleTemplate>("Assets/Resources/Prefab");
+        chatBlubble.chatBorder.sprite = uISetInfo.chatBorder;
+        EditorUtility.SetDirty(chatBlubble);
+
+        ShopItemFrame itemFrame = AssetDatabase.LoadAssetAtPath<ShopItemFrame>("Assets/Resources/Prefab");
+        itemFrame.bg.sprite = uISetInfo.itemframe;
+        EditorUtility.SetDirty(itemFrame);
+
+        Debug.Log("Skin Changed");
+    }
+
+}
+
---
+++ Assets/Script/WindowSystem/Shop/SkinSystem/UISetInfo.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName ="New UISkinInfo",menuName ="Skin/Scene/UISkinInfo")]
+public class UISetInfo : ScriptableObject
+{
+    [Header("Main Window Asset")]
+    public Sprite backGround;
+    public Sprite menuButton;
+    public Sprite menuBackground;
+    public WindowSkin[] windowSkins;
+    
+    [Header("Catbook Asset")]
+    public Sprite feedBackground;
+    public Sprite likeButton;
+    public Sprite catProfileBg;
+    public Sprite appProveButton;
+    public Sprite denyButton;
+
+    [Header("Chat Asset")]
+    public Sprite chatBorder;
+
+    [Header("ShopAsset")]
+    public Sprite itemframe;
+    public Sprite deskTopB;
+    public Sprite roomB;
+    public Sprite catB;
+    public Sprite foodB;
+
+    public WindowSkin GetSkinByType(WindowAppType appType)
+    {
+        foreach(WindowSkin skin in windowSkins)
+        {
+            if(appType == skin.type)
+            {
+                return skin;
+            }
+        }
+        Debug.LogError("Skin type Not found");
+        return new WindowSkin();
+    }
+}
+
+
+
+[System.Serializable]
+public struct WindowSkin
+{
+    public WindowAppType type;
+    public Sprite window;
+    public Sprite desktopIcon;
+    public Sprite closeIcon;
+}
+
